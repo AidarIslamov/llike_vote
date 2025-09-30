173767a5cf037270f80a11ed0e85b53ebc016492
aidar <aidar.islamov88@gmail.com>
2025-09-30 19:06:02 +0200

    added includes

--- backend/routes/idea.ts
+++ backend/routes/idea.ts
@@ -1,18 +1,35 @@
 import { FastifyInstance } from 'fastify'
+import { Idea } from '../models'
 
 export default async function ideaRoutes(fastify: FastifyInstance) {
-  fastify.get('/idea', async function handler(request, reply) {
-    return {
-      data: [
-        {
-          id: 1,
-          title: 'Test1'
-        },
-        {
-          id: 2,
-          title: 'Test2'
+  fastify.get('/idea', {
+    schema: {
+      querystring: {
+        type: 'object',
+        properties: {
+          include: { 
+            type: 'string',
+          }
         }
-      ]
+      }
+    },
+    handler: async (request, reply) => {
+      const { include } = request.query as { include?: string | string[] };
+      let includeArray: string[] = []
+
+      if (typeof include === 'string') {
+        includeArray = include.split(',')
+      } else if (Array.isArray(include)) {
+        includeArray = include
+      }
+      
+      if (includeArray.includes('votesCount')) {
+        const ideas = await Idea.scope('withVotesCount').findAll()
+        return { data: ideas }
+      } else {
+        const ideas = await Idea.findAll()
+        return { data: ideas }
+      }
     }
   })
 }
